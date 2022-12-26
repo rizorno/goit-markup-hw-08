86bdf301c03b9bc907ac05cf04185a42d9e65d63
rizorno <114782234+rizorno@users.noreply.github.com>
2022-12-26 18:05:30 +0100

    updatets

--- js/filter.js
+++ js/filter.js
@@ -18,17 +18,6 @@ refs.btns.addEventListener("click", (e) => {
   }
 });
 
-// window.addEventListener("click", (e) => {
-//   if (e.target.nodeName !== "BUTTON") {
-//     refs.btnAll.classList.add("active");
-//     let activeAll = document.querySelectorAll(".js-hidden");
-//     activeAll.forEach((element) => {
-//       element.classList.remove("js-hidden");
-//     });
-//     return;
-//   }
-// });
-
 refs.btnAll.onclick = () => {
   let activeAll = document.querySelectorAll(".js-hidden");
   activeAll.forEach((element) => {
@@ -37,62 +26,17 @@ refs.btnAll.onclick = () => {
   return;
 };
 
-// refs.btnWeb.onclick = () => {
-//   refs.listComponents.forEach((element) => {
-//     element.classList.add("js-hidden");
-//   });
-//   let activeDesign = document.querySelectorAll(".js-web");
-//   activeDesign.forEach((element) => {
-//     element.classList.remove("js-hidden");
-//   });
-// };
-
-// refs.btnApplication.onclick = () => {
-//   refs.listComponents.forEach((element) => {
-//     element.classList.add("js-hidden");
-//   });
-//   let activeList = document.querySelectorAll(".js-application");
-//   activeList.forEach((element) => {
-//     element.classList.remove("js-hidden");
-//   });
-// };
-
-// refs.btnDesign.onclick = () => {
-//   refs.listComponents.forEach((element) => {
-//     element.classList.add("js-hidden");
-//   });
-//   let activeList = document.querySelectorAll(".js-design");
-//   activeList.forEach((element) => {
-//     element.classList.remove("js-hidden");
-//   });
-// };
-
-// refs.btnMarketing.onclick = () => {
-//   refs.listComponents.forEach((element) => {
-//     element.classList.add("js-hidden");
-//   });
-//   let activeList = document.querySelectorAll(".js-marketing");
-//   activeList.forEach((element) => {
-//     element.classList.remove("js-hidden");
-//   });
-// };
-
 function sortList(nameSelector) {
   refs.listComponents.forEach((element) => {
-    element.classList.add("js-hidden");
-  });
-  let activeList = document.querySelectorAll(nameSelector);
-  console.log(activeList);
-
-  activeList.forEach((element) => {
-    element.classList.remove("js-hidden");
+    if (element.classList.contains(nameSelector)) {
+      element.classList.remove("js-hidden");
+    } else {
+      element.classList.add("js-hidden");
+    }
   });
 }
 
-refs.btnWeb.addEventListener("click", sortList(".js-web"));
-refs.btnApplication.addEventListener("click", sortList(".js-application"));
-
-// refs.btnWeb.onclick = sortList(".js-web");
-// refs.btnApplication.onclick = sortList(".js-application");
-// refs.btnDesign.onclick = sortList(".js-design");
-// refs.btnMarketing.onclick = sortList(".js-marketing");
+refs.btnWeb.addEventListener("click", () => sortList("js-web"));
+refs.btnApplication.addEventListener("click", () => sortList("js-application"));
+refs.btnDesign.addEventListener("click", () => sortList("js-design"));
+refs.btnMarketing.addEventListener("click", () => sortList("js-marketing"));
