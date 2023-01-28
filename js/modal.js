c5e240a457aa3a4a7def33395a303147b9450444
rizorno <114782234+rizorno@users.noreply.github.com>
2023-01-28 05:10:50 +0100

    updates

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,18 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+const refs = {
+  openModalBtn: document.querySelector("[data-modal-open]"),
+  closeModalBtn: document.querySelector("[data-modal-close]"),
+  modal: document.querySelector("[data-modal]"),
+};
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+refs.openModalBtn.addEventListener("click", openModal);
+refs.closeModalBtn.addEventListener("click", closeModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
+function openModal() {
+  refs.modal.classList.toggle("is-hidden");
+  document.querySelector("body").classList.add("js-body-scroll");
+}
+
+function closeModal() {
+  refs.modal.classList.toggle("is-hidden");
+  document.querySelector("body").classList.remove("js-body-scroll");
+}
