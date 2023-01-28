4914e18a268dc804348f30e2665b1ea277de4732
rizorno <114782234+rizorno@users.noreply.github.com>
2023-01-28 12:58:06 +0100

    updates

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,5 @@
 const refs = {
+  backdrop: document.querySelector(".backdrop"),
   openModalBtn: document.querySelector("[data-modal-open]"),
   closeModalBtn: document.querySelector("[data-modal-close]"),
   modal: document.querySelector("[data-modal]"),
@@ -10,9 +11,27 @@ refs.closeModalBtn.addEventListener("click", closeModal);
 function openModal() {
   refs.modal.classList.toggle("is-hidden");
   document.querySelector("body").classList.add("js-body-scroll");
+  refs.backdrop.addEventListener("click", closeModalClick);
+  window.addEventListener("keydown", closeModalEsc);
 }
 
 function closeModal() {
   refs.modal.classList.toggle("is-hidden");
   document.querySelector("body").classList.remove("js-body-scroll");
+  refs.backdrop.removeEventListener("click", closeModal);
+  window.removeEventListener("keypress", closeModalEsc);
+}
+
+function closeModalClick(e) {
+  if (e.target !== refs.backdrop) {
+    return;
+  }
+  closeModal();
+}
+
+function closeModalEsc(e) {
+  if (e.code === "Escape") {
+    closeModal();
+  }
+  return;
 }
